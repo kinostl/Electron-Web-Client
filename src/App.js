03a4d3f72e19d8d98bf8598747e509fbce3842ce
Minty <kinostl@gmail.com>
2019-06-10 01:49:28 -0400

    Navigation works

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react'
-import {MemoryRouter, Route} from 'react-router-dom'
+import { Link, Switch, MemoryRouter, Route } from 'react-router-dom'
 import ChatView from './views/chat_view'
 import SettingsView from './views/settings_view'
 
-class App extends React.Component{
-  render(){
+class App extends React.Component {
+  render() {
     return (
       <MemoryRouter>
-        <Route path="/" component={ChatView}/>
-        <Route path="/settings" component={SettingsView}/>
+        <nav>
+          <li><Link to="/">Worlds</Link></li>
+          <li><Link to="/settings">Settings</Link></li>
+        </nav>
+          <Route exact path="/" component={ChatView} />
+          <Route path="/settings" component={SettingsView} />
       </MemoryRouter>
-      
     )
   }
 }
