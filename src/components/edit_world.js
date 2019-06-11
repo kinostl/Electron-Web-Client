28e4b467584a910a1e7e004ddc7cfa7c6c29aaf3
Minty <kinostl@gmail.com>
2019-06-11 01:53:26 -0400

    Fixed rerender flashing bug

--- src/components/edit_world.js
+++ src/components/edit_world.js
@@ -8,75 +8,66 @@ import appState from '../store'
 class EditWorld extends React.Component {
     constructor() {
         super()
-        this.state={
-            "loading":true,
-            "world":{}
+        this.state = {
+            "world": {}
         }
         this.updateForm = this.updateForm.bind(this)
     }
 
-    updateForm(props){
+    updateForm(props) {
         appState.getWorldById(props.match.params.world_id).then((world) => {
             this.setState({
-                "loading":false,
-                "world":world
+                "world": world
             })
         })
-    
+
     }
 
-    componentDidMount(){
-        this.setState({"loading":true})
+    componentDidMount() {
         this.updateForm(this.props)
     }
 
-    componentDidUpdate(prevProps){
-        if(prevProps.match.params.world_id !== this.props.match.params.world_id){
-            this.setState({"loading":true})
+    componentDidUpdate(prevProps) {
+        if (prevProps.match.params.world_id !== this.props.match.params.world_id) {
             this.updateForm(this.props)
         }
     }
 
     render() {
-        if(this.state.loading){
-            return(
-                <span>Loading world</span>
-            )
-        }else{
-            return (
-                <Formik initialValues={this.state.world}
-                    onSubmit={appState.editWorld}
-                    render={({ errors, status, touched, isSubmitting }) => (
-                        <Form>
-                            <header className="App-header">
-                                Edit Character
+        return (
+            <Formik initialValues={this.state.world}
+                enableReinitialize={true}
+                onSubmit={appState.editWorld}
+                render={({ errors, status, touched, isSubmitting }) => (
+                    <Form>
+                        <header className="App-header">
+                            Edit Character
             </header>
-                            <label htmlFor="label">Label</label>
-                            <Field name="label" type="text" />
-                            <ErrorMessage name="label" />
-    
-                            <label htmlFor="name">Login Name</label>
-                            <Field name="name" type="text" />
-                            <ErrorMessage name="name" />
-                            <label htmlFor="password">Password</label>
-                            <Field name="password" type="password" />
-                            <ErrorMessage name="name" />
-    
-                            <label htmlFor="server_address">Server Address</label>
-                            <Field name="server_address" type="text" />
-                            <ErrorMessage name="server_url" />
-                            <label htmlFor="server_port">Server Port</label>
-                            <Field name="server_port" type="text" />
-                            <ErrorMessage name="server_port" />
-    
-                            <button type="submit" disabled={isSubmitting}>
-                                Save
+                        <label htmlFor="label">Label</label>
+                        <Field name="label" type="text" />
+                        <ErrorMessage name="label" />
+
+                        <label htmlFor="name">Login Name</label>
+                        <Field name="name" type="text" />
+                        <ErrorMessage name="name" />
+                        <label htmlFor="password">Password</label>
+                        <Field name="password" type="password" />
+                        <ErrorMessage name="name" />
+
+                        <label htmlFor="server_address">Server Address</label>
+                        <Field name="server_address" type="text" />
+                        <ErrorMessage name="server_url" />
+                        <label htmlFor="server_port">Server Port</label>
+                        <Field name="server_port" type="text" />
+                        <ErrorMessage name="server_port" />
+
+                        <button type="submit" disabled={isSubmitting}>
+                            Save
             </button>
-                        </Form>
-    
-                    )} />
-            )
-        }
+                    </Form>
+
+                )} />
+        )
     }
 }
 
